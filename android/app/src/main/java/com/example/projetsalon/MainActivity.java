b584c1701cc55310ed3e9c4b5ff002d674f4976d
SarahKaren4 <ykskaren@gmail.com>
2019-08-04 18:03:26 +0000

    Update MainActivity.java

--- android/app/src/main/java/com/example/projetsalon/MainActivity.java
+++ android/app/src/main/java/com/example/projetsalon/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.projetsalon;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import android.view.WindowManager.LayoutParams;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -12,3 +12,4 @@ public class MainActivity extends FlutterActivity {
     getWindow().addFlags(LayoutParams.FLAG_SECURE);
   }
 }
+
